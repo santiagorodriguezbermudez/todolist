a9b009a5a1174859c49f40bd02df8e1a7994102e
Nurislam Ab <you@example.com>
2020-09-03 23:16:24 +0500

    Add methods to add todo and project in the controller

--- src/controller.js
+++ src/controller.js
@@ -1,6 +1,40 @@
+import ToDo from './todo';
+import Project from './project';
+import View from './view';
 
-const controller = () => {
-  return 'hello controller';
-};
+const Controller = (() => {
+  const projectsArr = [];
 
-export default controller;
+  const addToDo = () => {
+    const title = document.getElementById('title').value;
+    const description = document.getElementById('description').value;
+    const dueDate = document.getElementById('due-date').value;
+    const priority = document.getElementById('priorities');
+    const selectedPriority = priority.options[priority.selectedIndex].text;
+    const project = document.getElementById('projects');
+    const selectedProject = project.options[project.selectedIndex].text;
+    const todo = ToDo(title, description, dueDate, selectedPriority);
+
+    const projectIndex = projectsArr.indexOf(selectedProject);
+    projectsArr[projectIndex].addTodoList(todo);
+  };
+
+  const addProject = () => {
+    const title = document.getElementById('project-title').value;
+    const project = Project(title);
+    projectsArr.push(project);
+  };
+
+  const start = () => {
+    console.log(projectsArr);
+    View.listProjects(projectsArr);
+  };
+
+  return {
+    addToDo,
+    addProject,
+    start,
+  };
+})();
+
+export default Controller;
