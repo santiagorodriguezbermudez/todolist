2a7ab4e3d4471374c109ea74f2e37e2e72229b19
Nurislam Ab <you@example.com>
2020-09-05 00:08:45 +0500

    Add methods to remove projects and todos from the local storage

--- src/storage.js
+++ src/storage.js
@@ -25,10 +25,27 @@ const AppLocalStorage = (() => {
     return projectIndex;
   };
 
+  const removeProject = (projectIndex) => {
+    const projects = parseData('projects');
+    projects.splice(projectIndex, 1);
+
+    localStorage.setItem('projects', JSON.stringify(projects));
+  };
+
+  const removeToDo = (projectIndex, toDoIndex) => {
+    const projects = parseData('projects');
+    const updatedProject = projects[projectIndex];
+    updatedProject.toDoList.splice(toDoIndex, 1);
+    projects.splice(projectIndex, 1, updatedProject);
+    localStorage.setItem('projects', JSON.stringify(projects));
+  };
+
   return {
     storeLocal,
     parseData,
     getProjectByTitle,
+    removeProject,
+    removeToDo,
   };
 })();
 
