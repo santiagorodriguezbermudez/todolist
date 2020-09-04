644556e06f2123fec57fe74e327814940a7d750e
Nurislam Ab <you@example.com>
2020-09-05 00:15:24 +0500

    Refactor code for the project factory

--- src/project.js
+++ src/project.js
@@ -1,27 +1,2 @@
-const Project = (title) => {
-  const todoList = [];
-
-  const editTitle = (title) => {
-    this.title = title;
-  };
-
-  const addTodoList = (todo) => {
-    todoList.push(todo);
-  };
-
-  const removeTodoFromProject = (toDoId) => {
-    todoList.splice(toDoId, 1);
-  };
-
-  const getTodosForProject = () => todoList;
-
-  return {
-    title,
-    addTodoList,
-    editTitle,
-    getTodosForProject,
-    removeTodoFromProject,
-  };
-};
-
+const Project = ((title, toDoList) => ({ title, toDoList }));
 export default Project;
